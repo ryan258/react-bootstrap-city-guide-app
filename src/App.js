ba267aa71a0bbe406d743426eebc8b173cc0b770
Ryan Johnson <ryan@258webco.com>
2020-05-09 14:30:57 -0500

    Install Bootstrap and create Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
-// import logo from './logo.svg';
+import React, { Component } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div>
-      <h1>hello city guide app</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1>hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
