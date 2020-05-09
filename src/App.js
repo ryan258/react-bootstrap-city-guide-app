9b2a151b07373c2e329c820ee69d726bdcc822f7
Ryan Johnson <ryan@258webco.com>
2020-05-09 16:08:42 -0500

    Add Footer Component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Navbar />
         <h1>hello</h1>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <h4>Lorem Ipsum</h4>
+            <ul className="list-unstyled">
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Lorem Ipsum</h4>
+            <ul className="list-unstyled">
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Lorem Ipsum</h4>
+            <ul className="list-unstyled">
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Lorem Ipsum</h4>
+            <ul className="list-unstyled">
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+              <li>Lorem Ipsum</li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p className="text-xs-center">
+            &copy;{new Date().getFullYear()} City Guide App - All Rights
+            Reserved
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
