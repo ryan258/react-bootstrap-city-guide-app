7da06ef23178f41db3fbc8a1763bae7f4cc0e02a
Ryan Johnson <ryan@258webco.com>
2020-05-14 14:06:53 -0500

    Develop NewsCard component and Map them into the News page. (NewsCard > News)

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import NewsCard from "../NewsCard";
+import { InfoConsumer } from "../context";
 
 class News extends Component {
   render() {
     return (
-      <div>
-        <h2>News Page</h2>
-      </div>
+      <InfoConsumer>
+        {/* <h2>News Page</h2> */}
+        {(value) => {
+          return value.news.map((item) => {
+            return <NewsCard key={item.id} item={item} />;
+          });
+        }}
+      </InfoConsumer>
     );
   }
 }
