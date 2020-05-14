61dfb578f389dd830de6922728db0d8f1ae8e93c
Ryan Johnson <ryan@258webco.com>
2020-05-14 14:59:28 -0500

    Build out 404 page.

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,13 +1,51 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 class NotFoundPage extends Component {
   render() {
     return (
-      <div>
-        <h2>NotFoundPage Page</h2>
-      </div>
+      <ComponentNotFound className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="error-template">
+              <h1>Whoops!</h1>
+              <h2>Error 404 - Page Not Found</h2>
+              <div className="error-details">
+                Sorry, nothing exists at this URL.
+              </div>
+              <div className="error-actions">
+                <Link to="/" className="btn btn-outline-primary btn-lg">
+                  <i className="fa fa-home" />
+                  &nbsp;Back to Main
+                </Link>
+                <Link to="/" className="btn btn-outline-secondary btn-lg">
+                  <i className="fa fa-envelope" />
+                  &nbsp;Support
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ComponentNotFound>
     );
   }
 }
 
 export default NotFoundPage;
+
+const ComponentNotFound = styled.div`
+  .error-template {
+    padding: 40px 15px;
+    text-align: center;
+  }
+
+  .error-actions {
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
+
+  .btn {
+    margin-right: 10px;
+  }
+`;
