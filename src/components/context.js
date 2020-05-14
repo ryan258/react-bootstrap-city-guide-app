59a52a8a7edf58704e637068f387eeb50ac01aa8
Ryan Johnson <ryan@258webco.com>
2020-05-14 10:42:11 -0500

    Use context to wire up and render newly created Info card components on the Home page.

--- src/components/context.js
+++ src/components/context.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+// 1.) import data from file
+import { placeInfo, reviews, detailInfo, news } from "../data";
 
 const InfoContext = React.createContext();
 
 // we're going to wrap the highest component with this (in the index.js)
 class InfoProvider extends Component {
+  // 2.) pull data from file into state
+  state = {
+    info: placeInfo,
+    reviews: reviews,
+    detailInfo: detailInfo,
+    news: news,
+  };
+
   render() {
     return (
-      <InfoContext.Provider value="Hello World! I am the DATA!">
+      // 3.) pass data through the Provider
+      <InfoContext.Provider
+        value={{
+          info: this.state.info,
+          reviews: this.state.reviews,
+          maps: this.state.maps,
+          headerTitle: this.state.headerTitle,
+          headerSubTitle: this.state.headerSubTitle,
+          headerText: this.state.headerText,
+          detailInfo: this.detailInfo,
+          news: this.state.news,
+          name: this.state.name,
+          avatar: this.state.avatar,
+          comment: this.state.comment,
+        }}
+      >
         {this.props.children}
       </InfoContext.Provider>
     );
