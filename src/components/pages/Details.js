5fc8a042eddcbb49b4c810419440e17e79133453
Ryan Johnson <ryan@258webco.com>
2020-05-14 13:10:40 -0500

    Develop and wire components to create the reviews section of the Details page. (ReviewCard > Reviews > Details)

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { InfoConsumer } from "../context";
 import styled from "styled-components";
+import Reviews from "../Reviews";
 
 class Details extends Component {
   render() {
@@ -101,7 +102,7 @@ class Details extends Component {
                   </div>
                   {/* Reviews */}
                   <div id="reviews" className="tab-pane" role="tabpanel">
-                    Reviews Content
+                    <Reviews />
                   </div>
                   {/* Map */}
                   <div id="map" className="tab-pane" role="tabpanel">
